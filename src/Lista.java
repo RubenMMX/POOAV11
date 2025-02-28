629929915a74633b71785b8cc81c50cc00cc5c82
RubenMMX <ruben.fermat@iesjulianmarias.es>
2025-02-28 20:30:42 +0100

    poo

--- src/Lista.java
+++ src/Lista.java
@@ -2,7 +2,7 @@ public class Lista {
     private int[] lista;
 
     public Lista() {
-
+        lista = new int[0];
     }
 
     public int elementos() {
@@ -14,65 +14,103 @@ public class Lista {
         for (int i = 0; i < this.lista.length; i++) {
             aux[i] = this.lista[i];
         }
-        aux[this.lista.length-1] = n;
+        aux[aux.length - 1] = n;
         this.lista = aux;
     }
 
-     public void insercionInicial(int n) {
-        int aux[] = new int[this.lista.length+1];
+    public void insercionInicial(int n) {
+        int aux[] = new int[this.lista.length + 1];
         for (int i = 0; i < this.lista.length; i++) {
-            aux[i+1] = this.lista[i];
+            aux[i + 1] = this.lista[i];
 
         }
         aux[0] = n;
         this.lista = aux;
 
-     }
+    }
 
-     public void isertarIndices(int indice,int n) {
-         int aux[] = new int[this.lista.length+1];
-         boolean insertado = false;
-        for (int i = 0; i < this.lista.length; i++) {
+    public void isertarIndices(int indice, int n) {
+
+        int aux[] = new int[this.lista.length + 1];
+
+        for (int i = 0; i < indice; i++) {
             aux[i] = this.lista[i];
-            if(this.lista[i] == indice) {
-                aux[i] = n;
-                insertado = true;
-                break;
-
-            }
         }
-        for(int i = indice; i < this.lista.length; i++) {
-            aux[i+1] = this.lista[i];
+        aux[indice] = n;
+
+        for (int i = indice; i < this.lista.length; i++) {
+            aux[i + 1] = this.lista[i];
         }
         this.lista = aux;
 
+    }
 
-     }
-     public int obtener(int indice) {
+    public int obtener(int indice) {
         return this.lista[indice];
 
     }
-    public void insertarlista(Lista l){
-        for(int i = 0; i < l.elementos(); i++) {
+
+    public void insertarlista(Lista l) {
+        for (int i = 0; i < l.elementos(); i++) {
             int numero = l.obtener(i);
             this.insercionFinal(numero);
         }
     }
-    public int buscar(int numero){
+
+    public int buscar(int numero) {
         int result = -1;
         for (int i = 0; i < this.lista.length; i++) {
-            if(this.lista[i] == numero) {
+            if (this.lista[i] == numero) {
                 result = i;
                 break;
             }
         }
         return result;
     }
+
     public void imprimir() {
         for (int i = 0; i < this.lista.length; i++) {
             System.out.print(this.lista[i] + " ");
         }
         System.out.println();
     }
+    public static Lista listaConcatena(Lista l1, Lista l2){
+        Lista aux = new Lista();
+        aux.insertarlista(l1);
+        aux.insertarlista(l2);
+        return aux;
+
+    }
+
+    public boolean equals(Object o){
+        Lista l = (Lista) o;
+        boolean iguales = true;
+        if(this.elementos() == l.elementos()){
+            for(int i = 0; i < this.lista.length; i++){
+                if(this.obtener(i) != l.obtener(i)){}
+            }
+
+        }else{
+            iguales = false;
+        }
+
+        return iguales;
+    }
+    public void eliminar(int posicion){
+        if(posicion >= 0 && posicion < this.lista.length){
+            int[] aux = new int[this.lista.length - 1];
+            for (int i = 0; i < this.lista.length; i++) {
+                if (i < posicion) {
+                    aux[i] = this.lista[i];
+                }else{
+                    aux[i] = this.lista[i+1];
+                }
+            }
+            this.lista = aux;
+        }
+
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Lista l = new Lista();
+        l.insercionFinal(1);
+        l.insercionFinal(2);
+        l.insercionFinal(3);
+        l.insercionFinal(4);
+        l.isertarIndices(1,99);
+        l.insercionInicial(0);
+        l.insercionInicial(-1);
+        l.imprimir();
+
+        Lista l2 = new Lista();
+        l2.insercionFinal(1);
+        l2.insercionFinal(2);
+        l2.insercionFinal(3);
+        l2.insercionFinal(4);
+
+
+
 
     }
 }
